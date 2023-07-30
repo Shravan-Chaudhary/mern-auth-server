5ca1918b46df045fba42e24a849f6e7110721e42
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-07-30 20:13:49 +0530

    Add userRoutes and userController

---
+++ backend/controllers/userController.js
@@ -0,0 +1,44 @@
+import asyncHandler from 'express-async-handler'
+
+// @desc    Auth user/set token
+// route    POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler (async (req, res) => {
+    res.status(200).json({message: 'Auth User'})
+})
+
+// @desc    Register a new User
+// route    POST /api/users
+// @access  Public
+const registerUser = asyncHandler (async (req, res) => {
+    res.status(200).json({message: 'Register User'})
+})
+
+// @desc    Logout User
+// route    POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler (async (req, res) => {
+    res.status(200).json({message: 'Logout User'})
+})
+
+// @desc    Get User Profile
+// route    GET /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler (async (req, res) => {
+    res.status(200).json({message: 'User Profile'})
+})
+
+// @desc    Update User Profile
+// route    PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler (async (req, res) => {
+    res.status(200).json({message: 'Update User Profile'})
+})
+
+export {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+}
