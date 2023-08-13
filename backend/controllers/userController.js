4ec520dd131fbe6eb70a39635f2929533d05ee1e
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-08-14 05:03:01 +0530

    Add JWT Generation and Cookie Saving

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,71 +1,67 @@
 import asyncHandler from 'express-async-handler'
-import mongoose from "mongoose";
-import User from "../models/userModel.js";
+import generateToken from '../utils/generateTokens.js'
+import mongoose from 'mongoose'
+import User from '../models/userModel.js'
 
 // @desc    Auth user/set token
 // route    POST /api/users/auth
 // @access  Public
-const authUser = asyncHandler (async (req, res) => {
-    res.status(200).json({message: 'Auth User'})
+const authUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Auth User' })
 })
 
 // @desc    Register a new User
 // route    POST /api/users
 // @access  Public
-const registerUser = asyncHandler (async (req, res) => {
-    const {name, email, password} = req.body
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
 
-    const userExists = await User.findOne({email})
+  const userExists = await User.findOne({ email })
 
-    if(userExists){
-        res.status(400)
-        throw new Error('User already exists')
-    }
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
 
-    const user = await User.create({
-        name,
-        email,
-        password
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    generateToken(res, user._id)
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
     })
-
-    if(user){
-        res.status(201).json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            password: user.password
-        })
-    } else{
-        res.status(400)
-        throw new Error('Invalid User Data')
-    }
+  } else {
+    res.status(400)
+    throw new Error('Invalid User Data')
+  }
 })
 
 // @desc    Logout User
 // route    POST /api/users/logout
 // @access  Public
-const logoutUser = asyncHandler (async (req, res) => {
-    res.status(200).json({message: 'Logout User'})
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Logout User' })
 })
 
 // @desc    Get User Profile
 // route    GET /api/users/profile
 // @access  Private
-const getUserProfile = asyncHandler (async (req, res) => {
-    res.status(200).json({message: 'User Profile'})
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'User Profile' })
 })
 
 // @desc    Update User Profile
 // route    PUT /api/users/profile
 // @access  Private
-const updateUserProfile = asyncHandler (async (req, res) => {
-    res.status(200).json({message: 'Update User Profile'})
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: 'Update User Profile' })
 })
 
-export {
-    authUser,
-    registerUser,
-    logoutUser,
-    getUserProfile,
-    updateUserProfile
-}
+export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }
--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,29 @@
-import express, {urlencoded} from 'express'
+import express, { urlencoded } from 'express'
 import dotenv from 'dotenv'
-import userRoutes from "./routes/userRoutes.js";
-import connectDB from "./config/db.js";
+import userRoutes from './routes/userRoutes.js'
+import connectDB from './config/db.js'
 
 dotenv.config()
-import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 const port = process.env.PORT || 5000
-connectDB()
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// MongoDb Connection
+connectDB()
 
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Routes
 app.use('/api/users/', userRoutes)
 app.get('/', (req, res) => res.send('Server is ready'))
 
+// Error handling
 app.use(notFound)
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`))
-
 
 // POST-  /api/users - Register a User
 // POST-  /api/users/auth - Authenticate a User and get token
@@ -27,3 +31,4 @@ app.listen(port, () => console.log(`Server is running on port: ${port}`))
 //GET-    /api/users/profile - Get User Profile
 //PUT-    /api/users/profile - Update User's Profile
 // 78-  https://10fastfingers.com/share-badge/1_CA
+// 83 - https://10fastfingers.com/share-badge/1_CF
