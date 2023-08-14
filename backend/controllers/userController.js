b4dafe08cd4a735f97a9972000c015a75e34ae87
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-08-15 03:17:12 +0530

    Logout and Destroy Cookie

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -61,7 +61,12 @@ const registerUser = asyncHandler(async (req, res) => {
 // route    POST /api/users/logout
 // @access  Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Logout User' })
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+
+  res.status(200).json({ message: 'User Logged Out' })
 })
 
 // @desc    Get User Profile
