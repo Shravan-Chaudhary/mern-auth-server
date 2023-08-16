4974722a3bdb0e8d3997c5fc8d33fd32ef6b240b
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-08-16 06:19:49 +0530

    reconfigure frontend with typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-5xl'>Hello</h1>
+      <Button variant='outline' size='lg'>
+        Hello
+      </Button>
+    </>
+  )
+}
+
+export default App
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,10 @@
 import path from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
     port: 3000,
     proxy: {
@@ -17,6 +12,11 @@ export default defineConfig({
         target: 'http://localhost:5000',
         changeOrigin: true,
       },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
